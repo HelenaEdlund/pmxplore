62b6b8272146a6a11d21cd167fc0c2108815c2cb
Helena Edlund <helena.edlund@astrazeneca.com>
2019-01-03 18:04:31 +0000

    made mode generic and added error message

--- R/utils.R
+++ R/utils.R
@@ -1,18 +1,22 @@
 #' @title Check for included columns
 #' @description Check for included columns
-#' @param .df dataframe to look in, Default: rawdata
-#' @param columns vector of columns that should be included, Default: all_cols
+#' @param vector1 vector of column names included
+#' @param vector2 vector of columns expected to be included
 #' @return message with Yes or a list of missing columns
 #' @rdname check_columns
 #' @export 
 #' @importFrom crayon blue
-check_columns <- function(.df = rawdata, columns = all_cols){
+check_columns <- function(vector1, vector2){
   
-  if(all(names(.df) %in% columns)){
+  if(!is.character(vector1) | !is.character(vector2)){
+    stop("One of the inputs are not a character vector")
+  }
+  
+  if(all(vector1 %in% vector2)){
     message("Yes")
   } else {
-    missing <- paste(names(.df)[!(names(.df) %in% columns)], collapse = ", ")
-    message("No, columns ", crayon::blue(missing), " are missing.")
+    missing <- paste(vector1[!vector1 %in% vector2], collapse = ", ")
+    message("No, ", crayon::blue(missing), " are missing.")
   }
   
 }
