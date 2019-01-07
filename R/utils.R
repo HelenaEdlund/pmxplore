8301e977db5b167c0fc326ed2f6fd6c5f437df38
Helena Edlund <helena.edlund@astrazeneca.com>
2019-01-07 19:02:25 +0000

    files merged

--- R/utils.R
+++ R/utils.R
@@ -88,3 +88,27 @@ rename_cols_mapping <- function(df, mapping_df){
   }
   return(df)
 }
+
+#' @title For printing a list of grobs to pdf
+#' @description forces a new page for grob objects to avoid printing all on the same page
+#' @param x grob grids
+#' @return "printed" grob object
+#' @seealso 
+#'  \code{\link[grid]{grid.newpage}},\code{\link[grid]{grid.draw}}
+#' @rdname grob_draw
+#' @export 
+#' @importFrom grid is.grob grid.newpage grid.draw
+
+grob_draw <- function(x){
+  
+  if(grid::is.grob(x)){
+    
+    grid::grid.newpage()
+    grid::grid.draw(x)
+    
+  } else {
+    
+    grid::grid.draw(x)
+    
+  }
+}
